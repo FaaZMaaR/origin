80cef41f07b5bab7465a91adbd3a59db48fb5ab6
FaaZMaaR <faazmaar@yandex.ru>
2024-11-02 16:42:34 +0300

    Добавлен Lesson 6

---
+++ Lesson 6/Task 1/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+#define MODE 1
+
+#ifndef MODE
+#error MODE is not defined!
+#elif MODE == 1
+int add(int a, int b) {
+	return a + b;
+}
+#endif
+
+int main() {
+	setlocale(LC_ALL, "rus");
+#if MODE == 0
+	std::cout << "Работаю в режиме тренировки" << std::endl;
+#elif MODE == 1
+	std::cout << "Работаю в боевом режиме" << std::endl;
+	std::cout << "Введите число 1: ";
+	int num1;
+	std::cin >> num1;
+	std::cout << "Введите число 2: ";
+	int num2;
+	std::cin >> num2;
+	std::cout << "Результат сложения: " << add(num1, num2) << std::endl;
+#else
+	std::cout << "Неизвестный режим. Завершение работы" << std::endl;
+#endif
+}
+
