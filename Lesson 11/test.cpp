59ea84d6dff7576699d74475c95f2eb13c8b8eb9
FaaZMaaR <faazmaar@yandex.ru>
2024-11-26 17:35:31 +0300

    Lesson 11: в тестах с проверкой на исключения REQUIRE заменены на REQUIRE_THROWS_AS

--- Lesson 11/test.cpp
+++ Lesson 11/test.cpp
@@ -8,6 +8,7 @@ TEST_CASE("Mean value") {
 		ExtArray<int> v1{ 4, 2, 7, 3, -5, 0, -3, 1 };
 		REQUIRE(v1.mean() == 1.125);
 	}
+
 	SECTION("empty array") {
 		ExtArray<int> v2{};
 		REQUIRE(v2.mean() == 0);
@@ -19,6 +20,7 @@ TEST_CASE("Median value") {
 		ExtArray<double> v1{ 4.5, 0.7, 10.8, -3.5 };
 		REQUIRE(v1.median() == 2.6);
 	}
+
 	SECTION("empty array") {
 		ExtArray<int> v2{};
 		REQUIRE(v2.median() == 0);
@@ -30,6 +32,7 @@ TEST_CASE("Mode value") {
 		ExtArray<int> v1{ 4, 2, 7, 2, 5, -5, 0, 5, 5 };
 		REQUIRE(v1.mode().first == 5);
 	}
+
 	SECTION("empty array") {
 		ExtArray<int> v2{};
 		REQUIRE(v2.mode().first == 0);
@@ -41,15 +44,10 @@ TEST_CASE("Mean with args") {
 		ExtArray<int> v1{ 4, 2, 7, 3, -5, 0, -3, 1 };
 		REQUIRE(v1.mean(2, 6) == 1.25);
 	}
+
 	SECTION("invalid_argument exception") {
-		try {
-			ExtArray<int> v2{ 4, 2, 7, 3, -5, 0, -3, 1 };
-			v2.mean(7, 3);
-			throw std::runtime_error("Something went wrong...");
-		}
-		catch (const std::exception& err) {
-			REQUIRE(typeid(err).name() == typeid(std::invalid_argument).name());
-		}
+		ExtArray<int> v2{ 4, 2, 7, 3, -5, 0, -3, 1 };
+		REQUIRE_THROWS_AS(v2.mean(7, 3), std::invalid_argument);
 	}
 }
 
@@ -58,28 +56,19 @@ TEST_CASE("CheckSum value") {
 		ExtArray<int> v1{ 1,0,0,1,1,1,0,1,0,0 };
 		REQUIRE(v1.checkSum() == 5);
 	}
+
 	SECTION("bool values") {
 		ExtArray<bool> v2{ false,false,true,false,true,false };
 		REQUIRE(v2.checkSum() == 2);
 	}
+
 	SECTION("bad_typeid exception") {
-		try {
-			ExtArray<double> v3{ 1,0,1,0,0 };
-			v3.checkSum();
-			throw std::runtime_error("Something went wrong...");
-		}
-		catch (const std::exception& err) {
-			REQUIRE(typeid(err).name() == typeid(std::bad_typeid).name());
-		}
+		ExtArray<double> v3{ 1,0,1,0,0 };
+		REQUIRE_THROWS_AS(v3.checkSum(), std::bad_typeid);
 	}
+
 	SECTION("logic_error exception") {
-		try {
-			ExtArray<int> v4{ 1,0,1,2,0 };
-			v4.checkSum();
-			throw std::runtime_error("Something went wrong...");
-		}
-		catch (const std::exception& err) {
-			REQUIRE(typeid(err).name() == typeid(std::logic_error).name());
-		}
+		ExtArray<int> v4{ 1,0,1,2,0 };
+		REQUIRE_THROWS_AS(v4.checkSum(), std::logic_error);
 	}
 }
