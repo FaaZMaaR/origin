c6ef27d288533d80ab4b5555ce9ad253f77f717e
FaaZMaaR <faazmaar@yandex.ru>
2025-01-18 15:39:43 +0300

    Добавлен Lesson 3

---
+++ Lesson 3/Task 1/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <catch2/catch_test_macros.hpp>
+
+#include <my_list.h>
+
+TEST_CASE("Test List", "[list]") {
+    SECTION("Test Empty()") {
+        List list{};
+        REQUIRE(list.Empty() == true);
+    }
+    SECTION("Test Size()") {
+        List list{};
+        list.PushBack(3);
+        list.PushBack(4);
+        list.PushFront(2);
+        list.PushFront(1);
+        REQUIRE(list.Size() == 4);
+    }
+    SECTION("Test Clear()") {
+        List list{};
+        list.PushBack(3);
+        list.PushBack(4);
+        list.PushFront(2);
+        list.PushFront(1);
+        list.Clear();
+        REQUIRE(list.Size() == 0);
+        REQUIRE(list.Empty() == true);
+    }
+}
---
+++ Lesson 3/Task 2/main.cpp
@@ -0,0 +1,47 @@
+﻿#include <catch2/catch_test_macros.hpp>
+
+#include <my_list.h>
+
+TEST_CASE("Test List", "[list]") {
+    SECTION("Test PushFront()") {
+        List list{};
+        list.PushFront(10);
+        REQUIRE(list.Size() == 1);
+    }
+    SECTION("Test PushBack()") {
+        List list{};
+        list.PushBack(20);
+        REQUIRE(list.Size() == 1);
+    }
+    SECTION("Test PopFront()") {
+        List list{};
+        list.PushFront(1);
+        list.PushBack(2);
+        REQUIRE(list.PopFront() == 1);
+    }
+    SECTION("Test PopBack()") {
+        List list{};
+        list.PushFront(1);
+        list.PushBack(2);
+        REQUIRE(list.PopBack() == 2);
+    }
+    SECTION("Test PopFront() for empty List") {
+        List list{};
+        REQUIRE_THROWS(list.PopFront());
+    }
+    SECTION("Test PopBack() for empty List") {
+        List list{};
+        REQUIRE_THROWS(list.PopBack());
+    }
+    SECTION("Complex Test List") {
+        List list{};
+        REQUIRE(list.Empty() == true);
+        list.PushBack(200);
+        list.PushFront(100);
+        REQUIRE(list.Size() == 2);
+        REQUIRE(list.PopBack() == 200);
+        REQUIRE(list.PopFront() == 100);
+        REQUIRE_THROWS(list.PopBack());
+        REQUIRE_THROWS(list.PopFront());
+    }
+}
