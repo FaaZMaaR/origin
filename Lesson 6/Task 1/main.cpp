4a85e55cff47aeca897eead2b369154f0464ce4e
FaaZMaaR <faazmaar@yandex.ru>
2025-01-28 17:42:52 +0300

    Добавлен Lesson 6

---
+++ Lesson 6/Task 1/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template<typename T>
+void unique_vector(std::vector<T>& vector) {
+    std::sort(vector.begin(), vector.end());
+    auto duplicates_iterator = std::unique(vector.begin(), vector.end());
+    vector.erase(duplicates_iterator, vector.end());
+}
+
+template<typename T>
+void display_vector(const std::vector<T>& vector) {
+    for (const auto& value : vector) {
+        std::cout << value << ' ';
+    }
+}
+
+int main() {
+    std::vector<int> int_vector{ 1,1,2,5,6,1,2,4 };
+    std::cout << "[IN]: ";
+    display_vector(int_vector);
+    unique_vector(int_vector);
+    std::cout << "\n[OUT]: ";
+    display_vector(int_vector);
+}
---
+++ Lesson 6/Task 2/main.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <set>
+#include <list>
+#include <vector>
+
+template<typename Iter>
+void print_container(Iter begin, Iter end) {
+    for (; begin != end; ++begin) {
+        std::cout << *begin << ' ';
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set.begin(), test_set.end());
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list.begin(), test_list.end());
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector.begin(), test_vector.end());
+}
