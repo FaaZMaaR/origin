7f3adaee349ef68600f0a58d37915a838a9e4fee
FaaZMaaR <faazmaar@yandex.ru>
2025-04-05 10:52:56 +0300

    Добавлен Lesson 3

---
+++ Lesson 3/Task 1/main.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <future>
+#include <vector>
+#include <random>
+
+template <typename Iter>
+void find_min(Iter beg, Iter end, std::promise<Iter> promise) {
+    Iter min = beg;
+    for (auto i = beg + 1; i != end; ++i) {
+        if (*i < *min) {
+            min = i;
+        }
+    }
+    promise.set_value(min);
+}
+
+template <typename Iter>
+void selection_sort(Iter beg, Iter end) {
+    for (auto i = beg; i != end; ++i) {
+        std::promise<Iter> promise{};
+        std::future<Iter> future{ promise.get_future() };
+        auto asyncFindMin = std::async(find_min<Iter>, i, end, std::move(promise));
+        std::swap(*i, *future.get());
+    }
+}
+
+int main() {
+    std::vector<int> vec(10);
+    std::random_device rd{};
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dis(0, 1000);
+    std::generate(vec.begin(), vec.end(), [=]()mutable {return dis(gen); });
+
+    std::cout << "Before sort:" << std::endl;
+    for (auto v : vec) {
+        std::cout << v << ' ';
+    }
+    selection_sort(vec.begin(), vec.end());
+    std::cout << "\n\nAfter sort:" << std::endl;
+    for (auto v : vec) {
+        std::cout << v << ' ';
+    }
+}
---
+++ Lesson 3/Task 2/main.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <future>
+#include <vector>
+
+
+template<typename Iter, typename Func>
+void parallel_for_each(Iter first, Iter last, Func func)
+{
+	unsigned long const length = std::distance(first, last);
+	unsigned long const max_chunk_size = 25;
+	if (length <= max_chunk_size)
+	{
+		std::for_each(first, last, func);
+	}
+	else
+	{
+		Iter mid_point = first;
+		std::advance(mid_point, length / 2);
+		std::future<void> first_half_result = std::async(parallel_for_each<Iter, Func>, first, mid_point, func);
+		std::future<void> second_half_result = std::async(parallel_for_each<Iter, Func>, mid_point, last, func);
+		first_half_result.wait();
+		second_half_result.wait();
+	}
+}
+
+int main()
+{
+	std::vector<int> V;
+	for (int i = 1; i <= 100; ++i) {
+		V.push_back(i);
+	}
+	parallel_for_each(V.begin(), V.end(), [](int value) {std::cout << value * value << ' '; });
+	return 0;
+}
