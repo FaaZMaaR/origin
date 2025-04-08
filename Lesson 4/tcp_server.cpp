9eff3bc7af8d0b1a92bf3892f9eb9fd3ba757072
FaaZMaaR <faazmaar@yandex.ru>
2025-04-08 21:40:09 +0300

    Добавлена многопоточность для TCP-сервера

--- Lesson 4/tcp_server.cpp
+++ Lesson 4/tcp_server.cpp
@@ -2,11 +2,13 @@
 #include <WS2tcpip.h>
 #include <iostream>
 #include <string>
+#include <thread>
 
 class TcpServer {
 private:
     WSAData wsadata;
     SOCKET mSocket;
+    SOCKET clientSocket;
     sockaddr_in server_addr{}, client_addr{};
     char* buf = new char[32];
     int port = 5555;
@@ -27,7 +29,8 @@ public:
 
         server_addr.sin_family = AF_INET;
         server_addr.sin_port = htons(port);
-        inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
+        server_addr.sin_addr.s_addr = ADDR_ANY;
+        //inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
 
         if (bind(mSocket, (SOCKADDR*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
             std::cout << "bind failed" << std::endl;
@@ -49,26 +52,49 @@ public:
         delete[] buf;
     }
 
+    void run() {
+        while (true) {
+            int client_size = sizeof(client_addr);
+            clientSocket = accept(mSocket, (SOCKADDR*)&client_addr, &client_size);
+            if (clientSocket == INVALID_SOCKET) {
+                closesocket(clientSocket);
+                WSACleanup();
+            }
+            std::thread(&TcpServer::client_handler, this, &clientSocket).detach();
+        }
+    }
+
+    void client_handler(SOCKET* clisock) {
+        std::string answer = "Hello, client";
+        int result = recv(*clisock, buf, 32, 0);
+        if (result > 0) {
+            std::cout << buf << std::endl;
+            send(*clisock, answer.c_str(), answer.size(), 0);
+        }
+    }
+
     void receive_and_answer() {
         SOCKET AcceptSocket;
         std::cout << "Waiting for client..." << std::endl;
 
-        AcceptSocket = accept(mSocket, NULL, NULL);
+        int client_size = sizeof(client_addr);
+
+        std::cout << inet_ntoa(client_addr.sin_addr) << std::endl;
+
+        AcceptSocket = accept(mSocket, (SOCKADDR*)&client_addr, &client_size);
         if (AcceptSocket == INVALID_SOCKET) {
             std::cout << "accept failed" << std::endl;
-            closesocket(mSocket);
-            WSACleanup();
-            return;
         }
         else {
             std::cout << "Client connected." << std::endl;
+            std::cout << inet_ntoa(client_addr.sin_addr) << std::endl;
         }
 
-        int size_data = recv(mSocket, buf, 32, 0);
+        int size_data = recv(AcceptSocket, buf, 32, 0);
         if (size_data > 0) {
             std::cout << buf << std::endl;
             std::string answer = "Hello, client!";
-            send(mSocket, answer.c_str(), answer.size(), 0);
+            send(AcceptSocket, answer.c_str(), answer.size(), 0);
         }
         else {
             std::cout << WSAGetLastError() << std::endl;
@@ -78,5 +104,5 @@ public:
 
 int main() {
     TcpServer server;
-    server.receive_and_answer();
+    server.run();
 }
